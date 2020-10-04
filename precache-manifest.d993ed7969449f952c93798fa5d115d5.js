7b7148d4c6bcff17af250b297a7292e07105e829
Vadim-w <vadim.morozov.2019@bk.ru>
2020-10-04 18:56:15 +0300

    Updates

---
+++ precache-manifest.d993ed7969449f952c93798fa5d115d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6aced28eee5c5f568a1cab115fe9b19",
+    "url": "/Cards-app/index.html"
+  },
+  {
+    "revision": "f9593e65d00f91cc0e65",
+    "url": "/Cards-app/static/css/main.fbdf563d.chunk.css"
+  },
+  {
+    "revision": "b22e3a0f5a7ce454a76c",
+    "url": "/Cards-app/static/js/2.1f19c700.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Cards-app/static/js/2.1f19c700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9593e65d00f91cc0e65",
+    "url": "/Cards-app/static/js/main.974a01ce.chunk.js"
+  },
+  {
+    "revision": "456259ff7247bd4f1534",
+    "url": "/Cards-app/static/js/runtime-main.dde5a214.js"
+  }
+]);
