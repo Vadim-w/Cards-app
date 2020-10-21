75a3fa5ea8a244309bd0ce376f906eb2c7fe22e6
Vadim-w <vadim.morozov.2019@bk.ru>
2020-10-21 11:19:38 +0300

    Added preloader.

--- src/n1-main/m1-ui/r3-Profile/ProfilePage.tsx
+++ src/n1-main/m1-ui/r3-Profile/ProfilePage.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import styles from "./ProfilePage.module.css"
+import {Preloader} from "../r7-common/preloader/Preloader";
 
 export const ProfilePage = () => {
     return (
         <div className={styles.profileBlock}>
+            <Preloader/>
             <h2>Profile</h2>
         </div>
     )
